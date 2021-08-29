ed61e41c21911479636ee1ca0b1cca41d31797be
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-08-29 15:19:01 +0000

    add back wasm file

--- src/pack_wasm.ts
+++ src/pack_wasm.ts
@@ -3,6 +3,6 @@ import { encode } from "../deps.ts";
 Deno.writeTextFileSync(
   "./src/wasm.js",
   `export const WASM_BASE64 = "${
-    encode(Deno.readFileSync("./src/canvaskit.wasm"))
+    encode(Deno.readFileSync("./src/canvaskit-opt.wasm"))
   }";`,
 );
